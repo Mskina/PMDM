41b0d45696024a39959822525d240b61036f95f0
Iván Estévez <mskina@gmail.com>
2023-03-24 11:29:53 +0100

    Creación ContadorExtra

---
+++ estevez_ivan_ex2tDist/app/src/main/java/com/example/estevez_ivan_ex2tDist/navigation/Navigation.kt
@@ -0,0 +1,23 @@
+package com.example.estevez_ivan_ex2tDist.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.estevez_ivan_ex2tDist.screens.*
+import com.example.estevez_ivan_ex2tDist.screens.ej01.Ej01Screen
+import com.example.estevez_ivan_ex2tDist.screens.ej02_tictactoe.Ej02Screen
+import com.example.estevez_ivan_ex2tDist.screens.ej03_calculadora.Ej03Screen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = Screens.MainScreen.route) {
+        composable(route = Screens.MainScreen.route) { MainScreen(navController) }
+        composable(route = Screens.Ejercicio01.route) { Ej01Screen() }
+        composable(route = Screens.Ejercicio02.route) { Ej02Screen() }
+        composable(route = Screens.Ejercicio03.route) { Ej03Screen() }
+
+    }
+}
+
---
+++ estevez_ivan_ex2tDist/app/src/main/java/com/example/estevez_ivan_ex2tDist/navigation/Screens.kt
@@ -0,0 +1,9 @@
+package com.example.estevez_ivan_ex2tDist.navigation
+
+sealed class Screens(val route: String){
+    object MainScreen: Screens("initial_screen")
+    object Ejercicio01: Screens("ej01")
+    object Ejercicio02: Screens("ej02")
+    object Ejercicio03: Screens("ej03")
+
+}
